50dcd358328b249a8d6ef42f3e51769819b4d3f6
Graham Flaspoehler <grahamflas@gmail.com>
2019-10-29 16:52:46 -0400

    MVP done, got through first 2 stretches

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('#create-task-form')
+  form.addEventListener("submit", handleSubmitEvent)
 });
+
+function handleSubmitEvent(event){
+  event.preventDefault();
+  console.log("We've reached our submit event.")
+  let newTask = document.querySelector('#new-task-description').value
+  let newPriority = document.querySelector('#priority').value
+
+  addToDo(newTask, newPriority)
+  event.target.reset();
+}
+
+function addToDo(newTask, newPriority) {
+  let ul = document.querySelector('#tasks');
+  let li = document.createElement('li');
+  let deleteButton = document.createElement('button');
+
+  if (newPriority === "high") {
+    li.className = "high";
+  } else if (newPriority === "medium"){
+    li.className = "medium"
+  } else {
+    li.className = "low"
+  }
+
+  li.innerText = newTask
+  deleteButton.innerText = "X"
+  deleteButton.id = "delete"
+  ul.appendChild(li)
+  li.appendChild(deleteButton)
+
+  deleteButton.addEventListener("click", removeToDo)
+};
+
+function removeToDo(event){
+  event.target.parentElement.remove()
+}
+
